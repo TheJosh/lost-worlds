8db27ecb719ec58558cc50bb668c285a4cae99b6
Josh Heidenreich <josh@karmabunny.com.au>
2017-08-23 13:34:22 +0930

    Capture mouse evnents; move keys var

--- src/events.js
+++ src/events.js
@@ -1,3 +1,11 @@
+var keys = { x: 0, y: 0 };
+var mouse = { x: 0, y: 0 };
+
+function mousemove(e) {
+	mouse.x = e.clientX;
+	mouse.y = e.clientY;
+}
+
 function keydown(e) {
 	switch (e.key) {
 		case 'ArrowDown': keys.y = 1; break;
--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ var PLAYER_SIZE = 20;
 var HALF_PLAYER_SIZE = 10;
 
 window.onresize = resizeRenderCanvas;
+window.mousemove = mousemove;
 window.onkeydown = keydown;
 window.onkeyup = keyup;
 
 var player = { x: 550, y: 550, rot: 0.5 * Math.PI };
 var accel = { x: 0, y: 0 };
-var keys = { x: 0, y: 0 };
 var gravSource = [
 	{ x: 4630, y: 70, dist: 0, strength: 0, dirY: 2 },
 	{ x: 4110, y: 1030, dist: 0, strength: 0, dirY: 2 }
