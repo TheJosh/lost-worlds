2a1db61b53500b5d6c1df50407df683a79f0ac85
Josh Heidenreich <josh.sickmate@gmail.com>
2017-08-23 19:22:31 +0930

    Update move keys to W-A-S-D

--- src/events.js
+++ src/events.js
@@ -8,18 +8,18 @@ function mousemove(e) {
 
 function keydown(e) {
 	switch (e.key) {
-		case 'ArrowDown': keys.y = 1; break;
-		case 'ArrowUp': keys.y = -1; break;
-		case 'ArrowLeft': keys.x = -1; break;
-		case 'ArrowRight': keys.x = 1; break;
+		case 'w': keys.y = -1; break;
+		case 's': keys.y = 1; break;
+		case 'a': keys.x = -1; break;
+		case 'd': keys.x = 1; break;
 	}
 }
 
 function keyup(e) {
 	switch (e.key) {
-		case 'ArrowDown':
-		case 'ArrowUp': keys.y = 0; break;
-		case 'ArrowLeft':
-		case 'ArrowRight': keys.x = 0; break;
+		case 'w':
+		case 's': keys.y = 0; break;
+		case 'a':
+		case 'd': keys.x = 0; break;
 	}
 }
