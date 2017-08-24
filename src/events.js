9b5503dd3e7d617a1de7eef89048d09d982f4ebd
Josh Heidenreich <josh@karmabunny.com.au>
2017-08-24 13:05:31 +0930

    Additional keys for jump and allow both arrows

--- src/events.js
+++ src/events.js
@@ -1,4 +1,4 @@
-var keys = { x: 0, y: 0 };
+var keys = { x: 0, y: 0, jump: 0 };
 var mouse = { x: 0, y: 0 };
 
 function mousemove(e) {
@@ -8,18 +8,20 @@ function mousemove(e) {
 
 function keydown(e) {
 	switch (e.key) {
-		case 'w': keys.y = -1; break;
-		case 's': keys.y = 1; break;
-		case 'a': keys.x = -1; break;
-		case 'd': keys.x = 1; break;
+		case 'w': case 'ArrowUp': keys.y = -1; break;
+		case 's': case 'ArrowDown': keys.y = 1; break;
+		case 'a': case 'ArrowLeft': keys.x = -1; break;
+		case 'd': case 'ArrowRight': keys.x = 1; break;
+		case ' ': keys.jump = 1; break;
 	}
 }
 
 function keyup(e) {
 	switch (e.key) {
-		case 'w':
-		case 's': keys.y = 0; break;
-		case 'a':
-		case 'd': keys.x = 0; break;
+		case 'w': case 'ArrowUp':
+		case 's': case 'ArrowDown': keys.y = 0; break;
+		case 'a': case 'ArrowLeft':
+		case 'd': case 'ArrowRight': keys.x = 0; break;
+		case ' ': keys.jump = 0; break;
 	}
 }
