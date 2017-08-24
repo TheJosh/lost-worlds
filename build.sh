7812a83cb33a363c0de2bf44735626423660db4a
Josh Heidenreich <josh.sickmate@gmail.com>
2017-08-24 21:39:36 +0930

    Build script to keep uncompressed files around

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # apt-get install closure-compiler
 
 
-mkdir -p tmp
+mkdir -p build
 
 cat \
 	src/globals.js \
@@ -12,25 +12,23 @@ cat \
 	src/engine/*.js \
 	src/entities/*.js \
 	src/main.js \
-	> tmp/all.js
+	> build/all.js
 
-closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS tmp/all.js >tmp/a.js
-rm tmp/all.js
+closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS build/all.js >build/a.js
+rm build/all.js
 
 echo "<style>body{margin:0;overflow:hidden}</style><canvas id=c></canvas><script src=a.js></script>" \
-	>tmp/index.html
+	>build/index.html
 
-pngcrush -q -brute -c 0 src/map.png tmp/map.png
+pngcrush -q -brute -c 0 src/map.png build/map.png
 
 rm -f build.zip
 
 echo "Would you like 50 more bytes? Remove this comment!" >archcomment
-zip -z -9 -q -j build.zip tmp/* <archcomment
+zip -z -9 -q -j build.zip build/* <archcomment
 rm archcomment
 
 SIZE=$( ls -al build.zip | cut -d ' ' -f 5 )
-
-rm -r tmp
 
 echo "Total size: $SIZE bytes"
 
