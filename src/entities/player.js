e74136fa19257bc7b2a9fdd4673f38000f86f53d
Josh Heidenreich <josh.sickmate@gmail.com>
2017-08-29 00:12:22 +0930

    Update system of player weapons to be variable stats

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,3 +1,24 @@
+var weapons = [
+    // Shotgun
+    {
+        vel: 400,
+        delay: 0.8
+    },
+
+    // Rifle
+    {
+        vel: 500,
+        delay: 0.1
+    },
+
+    // Pistol
+    {
+        vel: 700,
+        delay: 0.4
+    }
+];
+
+
 function Player(x, y) {
     this.x = x * universe.tileSize;
     this.y = y * universe.tileSize;
@@ -5,10 +26,11 @@ function Player(x, y) {
     this.vel = 0;
     this.weapon = 2;
 
-    var fireDelay = 0;
+    var fireWait = 0;
 
     var walkWobbleRot = 0;
     var walkWobbleDir = 0.01;
+
 
     var img = document.createElement('img');
     img.src = 'player.png';
@@ -28,7 +50,9 @@ function Player(x, y) {
     };
 
     this.update = function(delta) {
-        if (fireDelay > 0) fireDelay -= delta;
+        if (fireWait > 0) {
+            fireWait -= delta;
+        }
 
         if (keys.y != 0) {
             walkWobbleRot += walkWobbleDir;
@@ -39,7 +63,7 @@ function Player(x, y) {
     };
 
     this.fire = function(delta) {
-        if (fireDelay > 0) return;
+        if (fireWait > 0) return;
 
         var aheadPx = 40;
         var sidePx = 8;     // weapon is off-centre
@@ -47,9 +71,19 @@ function Player(x, y) {
         var x = this.x - (Math_cos(this.rot) * aheadPx) - (Math_cos(this.rot - Math_PI / 2) * sidePx);
         var y = this.y - (Math_sin(this.rot) * aheadPx) - (Math_sin(this.rot - Math_PI / 2) * sidePx);
 
-        bullets.push(
-            new Bullet(x, y, this.rot + Math_PI, 500 + player.vel)
-        );
-        fireDelay = 0.2;
+        if (this.weapon == 0) {
+            // Shotgun gets 4x extra bullets
+            for (var i = -2; i <= 2; ++i) {
+                bullets.push(
+                    new Bullet(x, y, this.rot + Math_PI + (i * 0.1), weapons[this.weapon].vel + player.vel)
+                );
+            }
+        } else {
+            bullets.push(
+                new Bullet(x, y, this.rot + Math_PI, weapons[this.weapon].vel + player.vel)
+            );
+        }
+
+        fireWait = weapons[this.weapon].delay;
     };
 }
