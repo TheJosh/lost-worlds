2c4fdaf7887b509c24b29df2f2f6f0c48b43c6cc
Josh Heidenreich <josh@karmabunny.com.au>
2017-08-30 16:00:33 +0930

    Intial line drawing between points of interest

--- src/engine/map.js
+++ src/engine/map.js
@@ -18,26 +18,33 @@ function generateNewMap() {
 	bullets.length = 0;
 	overlayWords.length = 0;
 
-	universe.mapWidth = 64;
-	universe.mapHeight = 64;
+	universe.mapWidth = 100;
+	universe.mapHeight = 100;
 
 	mapBuffer = new ArrayBuffer(universe.mapWidth * universe.mapHeight);
 	mapTiles = new Uint8Array(mapBuffer);
 	mapTiles.fill(1);
 
+	var pois = [];
+
 	var spawnX = Math_floor(getRandom(10, universe.mapWidth - 20));
 	var spawnY = Math_floor(getRandom(10, universe.mapHeight - 20));
-
 	player.spawn(spawnX, spawnY);
 	generateCircle(spawnX, spawnY, 8, 0);
+	pois.push({ x: spawnX, y: spawnY });
 
-	for (var i = 0; i < 8; ++i) {
+	for (var i = 0; i < 7; ++i) {
 		var x = Math_floor(getRandom(10, universe.mapWidth - 20));
 		var y = Math_floor(getRandom(10, universe.mapHeight - 20));
 		generateCircle(x, y, 8, 0);
+		pois.push({ x: x, y: y });
 	}
 
-	// TODO: Eventually the procedural generator goes here
+	while (pois.length >= 2) {
+		var a = pois.pop();
+		var b = pois.pop();
+		generateLine(a.x, a.y, b.x, b.y, 0);
+	}
 
 	postMapLoad();
 }
@@ -54,6 +61,37 @@ function generateCircle(originX, originY, radius, val)
 			if ((x * x + y * y) <= radiusSquared) {
 				setTile(originX + x, originY + y, val);
 			}
+		}
+	}
+}
+
+
+function generateLine(x0, y0, x1, y1, val)
+{
+	if (x1 < x0) x1 = [x0, x0 = x1][0];
+	if (y1 < y0) y1 = [y0, y0 = y1][0];
+
+	// Bresenham cannot handle vertical lines
+	if (x0 == x1) {
+		for (var y = y0; y <= y1; ++y) {
+			setTile(x0, y, val);
+		}
+		return;
+	}
+
+	var deltax = x1 - x0;
+	var deltay = y1 - y0;
+	var deltaerr = Math_abs(deltay / deltax);
+	var error = deltaerr - 0.5;
+
+	var y = y0;
+	for (var x = x0; x <= x1; ++x) {
+		setTile(x, y, val);
+
+		error += deltaerr;
+		if (error >= 0.5) {
+			y += 1;
+			error -= 1.0;
 		}
 	}
 }
