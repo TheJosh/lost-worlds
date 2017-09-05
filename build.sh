35509b46da5f9abc87ad31831d821e2a23835196
Josh Heidenreich <josh.sickmate@gmail.com>
2017-09-05 20:21:42 +0930

    Update build script

--- build.sh
+++ build.sh
@@ -18,8 +18,12 @@ cat \
 closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS --jscomp_off globalThis build/all.js >build/a.js
 rm build/all.js
 
-echo "<title>Lost Worlds</title><meta charset=\"UTF-8\"><style>body{margin:0;overflow:hidden}</style><canvas id=c></canvas><script src=a.js></script>" \
-	>build/index.html
+echo "<title>Lost Worlds</title>" >build/index.html
+echo "<meta charset=\"UTF-8\">" >>build/index.html
+echo "<meta name=viewport content=\"width=device-width,user-scalable=no\">" >>build/index.html
+echo "<style>body{margin:0;overflow:hidden}</style>" >>build/index.html
+echo "<canvas id=c></canvas>" >>build/index.html
+echo "<script src=a.js></script>" >>build/index.html
 
 pngcrush -q -brute src/p.png build/p.png
 advpng -z -i 100 -4 build/*.png
@@ -37,4 +41,3 @@ REMAIN=$( expr 13312 - $SIZE )
 
 echo "Used:   $SIZE bytes"
 echo "Spare:  $REMAIN bytes"
-
