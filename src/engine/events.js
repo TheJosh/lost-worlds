47243f3ea150ecd011f6e420cc7ea3514d0cd33d
Josh Heidenreich <josh.sickmate@gmail.com>
2017-09-05 21:30:20 +0930

    Reindet events.js

--- src/engine/events.js
+++ src/engine/events.js
@@ -5,102 +5,105 @@ var hasTouchEvents = ('createTouch' in document);
 
 
 if (!hasTouchEvents) {
-window.onmousemove = function(e) {
-	mouse.x = e.clientX;
-	mouse.y = e.clientY;
-};
+	// DESKTOP EVENTS
+	
+	window.onmousemove = function(e) {
+		mouse.x = e.clientX;
+		mouse.y = e.clientY;
+	};
 
-window.onmousedown = function(e) {
-	keys.fire = 1;
-};
-
-window.onmouseup = function(e) {
-	keys.fire = 0;
-};
-
-window.onkeydown = function(e) {
-	switch (e.key) {
-		case 'w': keys.y = -1; break;
-		case 's': keys.y = 1; break;
-		case 'c': player.changeWeapon(); break;
-	}
-};
-
-window.onkeyup = function(e) {
-	switch (e.key) {
-		case 'w':
-		case 's': keys.y = 0; break;
-	}
-};
-} else {
-
-
-function determineTouchType(t) {
-	if (t.clientY > (canvas.height - 150) && t.clientY < (canvas.height - 50)) {
-		if (t.clientX > 50 && t.clientX < 150) {
-			return 'fire';
-		} else if (t.clientX > 160 && t.clientX < 210) {
-			return 'weapon';
-		} else if (t.clientX > (canvas.width - 150) && t.clientX < (canvas.width - 50)) {
-			return 'aim';
-		}
-	}
-}
-
-window.ontouchstart = function(e) {
-	var type = determineTouchType(e.changedTouches[0]);
-	if (!type) return;
-
-	if (type == 'weapon') {
-		player.changeWeapon();
-		return;
-	}
-
-	touchId[type] = e.changedTouches[0].identifier;
-
-	if (type == 'fire') {
+	window.onmousedown = function(e) {
 		keys.fire = 1;
-	}
-};
+	};
 
-window.ontouchend = function(e) {
-	for (type in touchId) {
-		if (touchId[type] == e.changedTouches[0].identifier) {
-			touchId[type] = null;
-		}
-	}
-
-	if (touchId.fire === null) {
+	window.onmouseup = function(e) {
 		keys.fire = 0;
-	}
+	};
 
-	if (touchId.aim === null) {
-		keys.y = 0;
-	}
-};
+	window.onkeydown = function(e) {
+		switch (e.key) {
+			case 'w': keys.y = -1; break;
+			case 's': keys.y = 1; break;
+			case 'c': player.changeWeapon(); break;
+		}
+	};
+
+	window.onkeyup = function(e) {
+		switch (e.key) {
+			case 'w':
+			case 's': keys.y = 0; break;
+		}
+	};
 
 
-window.ontouchmove = function(e) {
-	e.preventDefault();
+} else {
+	// MOBILE EVENTS
 
-	for (i = 0; i < e.changedTouches.length; ++i) {
-		var t = e.changedTouches.item(i);
-
-		if (touchId.aim === t.identifier) {
-			// Aim - virtual joystick
-			var x = canvas.width - t.clientX - 100;
-			var y = canvas.height - t.clientY - 100;
-
-			mouse.x = (canvas.width / 2) - (x * 2);
-			mouse.y = (canvas.height / 2) - (y * 2);
-
-			var distSq = (x * x) + (y * y);
-			if (distSq > (25 * 25)) {
-				keys.y = -1;
-			} else {
-				keys.y = 0;
+	function determineTouchType(t) {
+		if (t.clientY > (canvas.height - 150) && t.clientY < (canvas.height - 50)) {
+			if (t.clientX > 50 && t.clientX < 150) {
+				return 'fire';
+			} else if (t.clientX > 160 && t.clientX < 210) {
+				return 'weapon';
+			} else if (t.clientX > (canvas.width - 150) && t.clientX < (canvas.width - 50)) {
+				return 'aim';
 			}
 		}
 	}
-};
+
+	window.ontouchstart = function(e) {
+		var type = determineTouchType(e.changedTouches[0]);
+		if (!type) return;
+
+		if (type == 'weapon') {
+			player.changeWeapon();
+			return;
+		}
+
+		touchId[type] = e.changedTouches[0].identifier;
+
+		if (type == 'fire') {
+			keys.fire = 1;
+		}
+	};
+
+	window.ontouchend = function(e) {
+		for (type in touchId) {
+			if (touchId[type] == e.changedTouches[0].identifier) {
+				touchId[type] = null;
+			}
+		}
+
+		if (touchId.fire === null) {
+			keys.fire = 0;
+		}
+
+		if (touchId.aim === null) {
+			keys.y = 0;
+		}
+	};
+
+	window.ontouchmove = function(e) {
+		e.preventDefault();
+
+		for (i = 0; i < e.changedTouches.length; ++i) {
+			var t = e.changedTouches.item(i);
+
+			if (touchId.aim === t.identifier) {
+				// Aim - virtual joystick
+				var x = canvas.width - t.clientX - 100;
+				var y = canvas.height - t.clientY - 100;
+
+				mouse.x = (canvas.width / 2) - (x * 2);
+				mouse.y = (canvas.height / 2) - (y * 2);
+
+				var distSq = (x * x) + (y * y);
+				if (distSq > (25 * 25)) {
+					keys.y = -1;
+				} else {
+					keys.y = 0;
+				}
+			}
+		}
+	};
 }
