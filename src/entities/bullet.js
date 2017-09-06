76d5b484f5aed48122f7eb83da1a4d0f522baa75
Josh Heidenreich <josh.sickmate@gmail.com>
2017-09-06 22:12:54 +0930

    Bullet reindent

--- src/entities/bullet.js
+++ src/entities/bullet.js
@@ -9,30 +9,32 @@ function Bullet(x, y, rot, vel) {
     this.hitCount = 0;
 }
 
-    Bullet.prototype.render = function(ctx) {
-        ctx.globalAlpha = this.strength;
-        ctx.fillRect(this.x - 2, this.y - 2, 4, 4);
-    };
 
-    Bullet.prototype.update = function(delta) {
-        this.x += this.dir.x * delta;
-        this.y += this.dir.y * delta;
+Bullet.prototype.render = function(ctx) {
+    ctx.globalAlpha = this.strength;
+    ctx.fillRect(this.x - 2, this.y - 2, 4, 4);
+};
 
-        if (this.x < 0 || this.y < 0 || this.x > universe.mapWidth * universe.tileSize || this.y > universe.mapHeight * universe.tileSize) {
-            this.alive = false;
-        }
 
-        checkCollideTiny(this, function() {
-            if (this.hitCount++ > 3) this.alive = false;
-            if (Math_random() > 0.7) this.alive = false;
+Bullet.prototype.update = function(delta) {
+    this.x += this.dir.x * delta;
+    this.y += this.dir.y * delta;
 
-            this.rot += getRandom(0, Math_PI * 2);
-            this.dir = getRotVel(this.rot, this.vel);
-            this.strength -= 0.1;
-        });
+    if (this.x < 0 || this.y < 0 || this.x > universe.mapWidth * universe.tileSize || this.y > universe.mapHeight * universe.tileSize) {
+        this.alive = false;
+    }
 
-        this.strength -= 0.01;
-        if (this.strength < 0.01) {
-            this.alive = false;
-        }
-    };
+    checkCollideTiny(this, function() {
+        if (this.hitCount++ > 3) this.alive = false;
+        if (Math_random() > 0.7) this.alive = false;
+
+        this.rot += getRandom(0, Math_PI * 2);
+        this.dir = getRotVel(this.rot, this.vel);
+        this.strength -= 0.1;
+    });
+
+    this.strength -= 0.01;
+    if (this.strength < 0.01) {
+        this.alive = false;
+    }
+};
