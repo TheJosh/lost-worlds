9cd7c2dbb9a27ee348bbda345dde532ead94c09c
Josh Heidenreich <josh.sickmate@gmail.com>
2017-09-08 19:41:45 +0930

    Update tutorial map

--- src/init_map.js
+++ src/init_map.js
@@ -1,5 +1,5 @@
 var init_map = '\
-1100000000001111111111111111111111111000001111111\
+1100000000001111111111111111111111111111111111111\
 1000000000000111111111111111111111110000000111111\
 0000000000000011111111111111111110000000000001111\
 0000000000000011111111111111111100000000000000111\
@@ -46,9 +46,9 @@ function loadInitMap()
 
 	player.spawn(6, 7);
 	gravSource.push(new BlackHole(12, 34));
-	enemies.push(new Enemy(26, 7, ENEMY_BEHAVE_VERT));
-	enemies.push(new Enemy(30, 5, ENEMY_BEHAVE_VERT));
+	enemies.push(new Enemy(38, 5, ENEMY_BEHAVE_VERT));
 	enemies.push(new Enemy(41, 26, ENEMY_BEHAVE_HORIZ));
+	enemies.push(new Enemy(42, 28, ENEMY_BEHAVE_HORIZ));
 	collectables.push(new Collectable(37, 13));
 
 	for (i = 0; i < init_map.length; ++i) {
@@ -60,9 +60,12 @@ function loadInitMap()
 	}
 
 	// Tutorial
-	overlayWords.push(new WordStatic(1200, 310, 'Collect electronics'));
-	overlayWords.push(new WordStatic(1105, 365, 'Repair teleporters'));
-	overlayWords.push(new WordStatic(1160, 494, 'Find a way home'));
+	overlayWords.push(new WordStatic(470, 110, '... somewhere deep underground near the Large Hadron Collider...'));
+	overlayWords.push(new WordStatic(600, 280, '... something has gone terribly wrong...'));
+	overlayWords.push(new WordStatic(1250, 90, '<-- Watch out'));
+	overlayWords.push(new WordStatic(1225, 425, '<-- Collect electronics'));
+	overlayWords.push(new WordStatic(530, 1050, '<-- Try to not get lost'));
+	overlayWords.push(new WordStatic(560, 1070, 'in the multiverse'));
 
 	postMapLoad();
 }
