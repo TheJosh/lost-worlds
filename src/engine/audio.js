726844fbb3068878c9cc676f8e792de9ae8b8d96
Josh Heidenreich <josh@karmabunny.com.au>
2017-09-13 06:53:06 +0930

    Sound playing ALWAYS via legacy
    I've abandoned the Web Audio API plans

--- src/engine/audio.js
+++ src/engine/audio.js
@@ -1,14 +1,3 @@
-var playSound;
-
-var audioCtx = window.AudioContext || window.webkitAudioContext;
-if (audioCtx) {
-    audioCtx = new audioCtx();
-    playSound = playSound_AudioAPI;
-} else {
-    playSound = playSound_Legacy;
-}
-
-
 // Preload sounds
 (function() {
 	var sounds = '47acx';
@@ -18,13 +7,7 @@ if (audioCtx) {
 })();
 
 
-function playSound_AudioAPI(name, rate)
-{
-    playSound_Legacy(name, rate);
-}
-
-
-function playSound_Legacy(name, rate)
+function playSound(name, rate)
 {
     var snd = new Audio(name + '.mp3');
     snd.playbackRate = rate;
